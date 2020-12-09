c00905015b74ba9be08213109461c4c2c199847b
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-09 15:17:54 -0800

    tests pass

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Mitch"
+    end
+
+    get '/hometown' do
+        "My hometown is Gig Harbor"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Fake plastic trees"
+    end
 end
